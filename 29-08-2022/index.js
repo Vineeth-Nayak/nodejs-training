bf17a5dceb1dd328040ab967f4e7f4f4e5217e62
Vineeth-Nayak <vineeth.pnayak@robosoft.com>
2022-08-29 14:23:45 +0530

    path module - globals

--- 29-08-2022/index.js
+++ 29-08-2022/index.js
@@ -16,5 +16,21 @@ exports.sad = () => {
 
 // console.log(module.exports);
 
-// only give file name
+// only give file name //! for consistant results add path.win2.basename on all OS
 console.log(path.basename(__filename), EOL);
+console.log(path.basename(__filename, ".js"), EOL); // gives only "index" slices ".js"
+
+// gives directory of the spicified file
+console.log(path.dirname(__filename), EOL);
+
+// gives extension of the spicified file
+console.log(path.extname(__filename), EOL);
+
+// givesobject form of path
+console.log(path.parse(__filename), EOL);
+
+// file seperator
+console.log(__filename.split(path.sep), EOL);
+
+// joins path
+console.log(path.join("fasd", "asdasd"));
