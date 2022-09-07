a57b260cd97a22a77b4efc318e56cac0d9b03e28
Vineeth-Nayak <vineeth.pnayak@robosoft.com>
2022-09-07 09:57:47 +0530

    mongo Queires

---
+++ mongoQueries.js
@@ -0,0 +1,53 @@
+// ! insert many document
+db.books.insertMany([
+  {
+    title: "Hello Sarr",
+    author: "heludilla",
+    pages: 230,
+    rating: 10,
+    genres: ["sci-fi", "dystopian"],
+  },
+  {
+    title: "Hello Sarr 1",
+    author: "heludilla 1",
+    pages: 1000,
+    rating: 5,
+    genres: ["fantasy"],
+  },
+]);
+
+// ! insert one document
+db.books.insertOne({
+  title: "Hello Sarr",
+  author: "heludilla",
+  pages: 230,
+  rating: 10,
+  genres: ["sci-fi", "dystopian"],
+});
+
+// *------------------------------------------------------
+// ! find all documents
+db.books.find(); // return only 1st 20 in mongoShell
+
+// ! find all documents that match the filter
+db.books.find({ author: "heludilla" });
+// or
+db.books.find({ author: "heludilla", rating: 10 });
+
+// ! find all documents that match the filter and send only specific field
+db.books.find({ author: "heludilla" }, { title: 1, author: 1 });
+
+// *------------------------------------------------------
+// ? COMPLEX QUERIES
+// ! find using or ("or" takes array of filters)
+db.books.find({ $or: [{ author: "heludilla" }, { rating: 2 }] });
+
+// ! find  rating greater than 8 or less than 10
+db.books.find({ $or: [{ rating: { $gt: 8 } }, { rating: { $lt: 10 } }] });
+
+// !find array element containing specified element
+db.books.find({ genres: "fantasy" });
+
+// !find array element containing "exactly" the specified element
+db.books.find({ genres: ["fantasy"] });
+db.books.find({ genres: ["fantasy", "sci-fi"] });
