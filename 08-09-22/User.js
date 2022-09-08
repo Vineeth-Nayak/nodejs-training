6af8b06808aa92fe31609aae296c17dea9d9b48f
Vineeth-Nayak <vineeth.pnayak@robosoft.com>
2022-09-08 14:16:40 +0530

    db done

--- 08-09-22/User.js
+++ 08-09-22/User.js
@@ -5,8 +5,20 @@ const userSchema = new Schema({
   name: { type: String, required: true },
   dateOfBirth: { type: String, required: true },
   email: { type: String, required: true },
-  password: { type: String, required: true },
-  address: [{ type: String, required: true }],
+  password: { type: String, required: true, minLength: 8 },
+  address: [
+    {
+      type: String,
+      required: true,
+      validate: {
+        validator: function () {
+          console.log(this.address.length, "asdasdasdasdasdasdsdasd");
+          return !(this.address.length > 5);
+        },
+        message: (props) => `${props.value} exceeds maximum array size (10)!`,
+      },
+    },
+  ],
 });
 
 module.exports = mongoose.model("User", userSchema);
--- 08-09-22/dbConn.js
+++ 08-09-22/dbConn.js
@@ -1,2 +1,46 @@
 const mongoose = require("mongoose");
+const User = require("./User");
+
 mongoose.connect("mongodb://localhost/task1");
+
+run();
+
+async function run() {
+  try {
+    const user = new User({
+      name: "Vin",
+      dateOfBirth: "12/01/2001",
+      email: "vin@gmail.com",
+      password: "123123123",
+      address: [
+        "asdasd asdasd asdasd",
+        "asdasd asdasd asdasd",
+        "asdasd asdasd asdasd",
+        "asdasd asdasd asdasd",
+        // "asdasd asdasd asdasd",
+      ],
+    });
+    user.save((err, docs) => {
+      console.log(docs);
+      addAddress(docs._id);
+    });
+  } catch (error) {
+    console.log("Error has occured");
+  }
+}
+
+async function addAddress(_id) {
+  try {
+    console.log(_id);
+    User.updateOne(
+      { _id, $expr: { $lt: [{ $size: "$address" }, 5] } },
+
+      { $push: { address: "address hello" } },
+      (err, docs) => {
+        console.log("DOcs maccha", docs);
+      }
+    );
+  } catch (error) {
+    console.log("Error has occured from addAddress", error);
+  }
+}
