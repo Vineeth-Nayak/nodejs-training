2b7307e08b41d3fd172311c025b1aafe4f0c2e40
Vineeth-Nayak <vineeth.pnayak@robosoft.com>
2022-09-09 14:53:11 +0530

    setup of structures

--- mongoDb/index.js
+++ mongoDb/index.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const port = process.env.PORT | 5000;
 
+// db Connection
 const databaseConnect = require("./config/dbConn");
 
-const userRouter = require("./routes/");
+// routes
+const userRouter = require("./routes/Users/user");
+
+app = express();
+router.use(express.urlencoded({ extended: true }));
+router.use(express.json());
+
+databaseConnect();
+
+app.use("/", router);
+app.use("api/user", userRouter);
+
+app.listen(port, () => console.log("Running port", port));
